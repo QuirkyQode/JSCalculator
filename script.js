2bc53ea79aa024cca37a26a94a2b97e4bdfa2c9a
Curiousss <19431414+Curiousss@users.noreply.github.com>
2022-10-16 19:47:44 +0530

    handling negation and decimal numbers

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ let clear = document.querySelector(".clear")
 let back = document.querySelector(".back")
 let equal = document.querySelector(".op-equal")
 let neg = document.querySelector(".op-neg")
+let dec = document.querySelector(".op-dec")
+
+
+//  ["init", "op1", "final" ];
+// ["nodec", "dec1", "dec2", "dec12"]
+let decstate = "nodec"
+let curstate = "init";
+let negstate = false;
+let op;
+
 
 num.forEach((n) => {
     n.addEventListener('click', () => {
         if(curstate == "final") {
             curstate = "init"
+            negstate = false
             display.innerText = n.innerText
         }
         else {
@@ -19,9 +30,6 @@ num.forEach((n) => {
 }
 );
 
-//  ["init", "op1", "op", "dec", "final" ];
-let curstate = "init";
-let op;
 ops.forEach((o) => {
     o.addEventListener('click', () => {
         if(curstate == "init") {
@@ -37,9 +45,29 @@ equal.addEventListener('click', () => {
     if(op != undefined) {
         vals = display.innerText.split(op);
         console.log(vals);
+        let numfunc1;
+        let numfunc2;
+        switch (decstate) {
+            case "nodec":
+                numfunc1 = Number;
+                numfunc2 = Number;
+                break;
+            case "dec1":
+                numfunc1 = parseFloat;
+                numfunc2 = Number;
+                break;
+            case "dec2":
+                numfunc1 = Number;
+                numfunc2 = parseFloat;
+                break;
+            case "dec12":
+                numfunc1 = parseFloat;
+                numfunc2 = parseFloat;
+                break;
+        }
         switch(op){
             case '+':
-                display.innerText = Number(vals[0]) + Number(vals[1])
+                display.innerText = numfunc1(vals[0]) + numfunc2(vals[1])
                 break
             case '-':
                 display.innerText = Number(vals[0]) - Number(vals[1])
@@ -56,10 +84,35 @@ equal.addEventListener('click', () => {
     op = undefined;
 });
 
+dec.addEventListener('click', () => {
+    if(curstate == "init" && decstate == "nodec") {
+        display.innerText = display.innerText + "."
+        decstate = "dec1"
+    }
+    else if(curstate == "op1" && (decstate == "nodec" || decstate == "dec1")) {
+        display.innerText = display.innerText + "."
+        if(decstate == "nodec") {
+            decstate = "dec2"
+        }
+        else if (decstate == "dec1") {
+            decstate = "dec12"
+        }
+    }
+});
+
+neg.addEventListener('click', () => {
+    if (negstate == false) {
+        display.innerText = "-" + display.innerText
+        negstate = true
+    }
+});
+
 back.addEventListener('click', () => {
     display.innerText = display.innerText.slice(0, -1)
 });
 
 clear.addEventListener('click', () => {
     display.innerText = ""
+    curstate = "final"
+    negstate = false
 });
